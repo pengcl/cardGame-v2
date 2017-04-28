c8b79594b73554ad2a21c885744f2346455262dc
pengcl <3559996@qq.com>
2017-04-28 18:18:57 +0800

    rebiuld

--- server/imports/fixtures/players.ts
+++ server/imports/fixtures/players.ts
@@ -12,9 +12,92 @@ let fake = {
     score: function (start: number, end: number) {
         return start + Math.ceil((end - start) * Math.random());
     },
+    ability: function (type: string) {
+        if (type == 'attack') {
+            return {
+                attack: this.score(80, 100),
+                defense: this.score(50, 80),
+                speed: this.score(70, 100),
+                skill: this.score(70, 100),
+                strength: this.score(70, 100),
+                spirit: this.score(70, 100)
+            }
+        }
+        if (type == 'Midfield') {
+            return {
+                attack: this.score(70, 90),
+                defense: this.score(70, 90),
+                speed: this.score(70, 100),
+                skill: this.score(70, 100),
+                strength: this.score(70, 100),
+                spirit: this.score(70, 100)
+            }
+        }
+        if (type == 'defense') {
+            return {
+                attack: this.score(50, 80),
+                defense: this.score(80, 100),
+                speed: this.score(70, 100),
+                skill: this.score(70, 100),
+                strength: this.score(70, 100),
+                spirit: this.score(70, 100)
+            }
+        }
+    },
     position: function () {
-        let positions = ['ST', 'AM', 'LW', 'RW', 'LM', 'CM', 'RM', 'LB', 'CB', 'RB', 'GK'];
-        return positions[Math.ceil(Math.random() * 10)];
+        let ability = this.ability(['attack', 'Midfield', 'defense'][this.score(0, 2)]);
+
+        let position = [
+            {
+                name: 'ST',
+                score: (ability.attack * 1.2 + ability.defense * 0.8 + ability.speed + ability.skill + ability.strength + ability.spirit) / 6,
+            },
+            {
+                name: 'AML',
+                score: (ability.attack * 1.1 + ability.defense * 0.8 + ability.speed * 1.1 + ability.skill + ability.strength + ability.spirit) / 6
+            },
+            {
+                name: 'AMF',
+                score: (ability.attack * 1.1 + ability.defense * 0.9 + ability.speed + ability.skill + ability.strength + ability.spirit) / 6
+            },
+            {
+                name: 'AMR',
+                score: (ability.attack * 1.1 + ability.defense * 0.8 + ability.speed * 1.1 + ability.skill + ability.strength + ability.spirit) / 6
+            },
+            {
+                name: 'LMF',
+                score: (ability.attack * 1 + ability.defense * 1 + ability.speed * 1.05 + ability.skill + ability.strength + ability.spirit) / 6
+            },
+            {
+                name: 'CMF',
+                score: (ability.attack * 1 + ability.defense * 1 + ability.speed * 1 + ability.skill * 1.05 + ability.strength + ability.spirit) / 6
+            },
+            {
+                name: 'RMF',
+                score: (ability.attack * 1 + ability.defense * 1 + ability.speed * 1.05 + ability.skill + ability.strength + ability.spirit) / 6
+            },
+            {
+                name: 'DMF',
+                score: (ability.attack * 0.9 + ability.defense * 1.1 + ability.speed + ability.skill + ability.strength + ability.spirit) / 6
+            },
+            {
+                name: 'LB',
+                score: (ability.attack * 0.8 + ability.defense * 1.1 + ability.speed * 1.1 + ability.skill + ability.strength + ability.spirit) / 6
+            },
+            {
+                name: 'CB',
+                score: (ability.attack * 0.9 + ability.defense * 1.1 + ability.speed + ability.skill + ability.strength + ability.spirit) / 6
+            },
+            {
+                name: 'RB',
+                score: (ability.attack * 0.8 + ability.defense * 1.1 + ability.speed * 1.1 + ability.skill + ability.strength + ability.spirit) / 6
+            },
+            {
+                name: 'GK',
+                score: (ability.attack * 0.5 + ability.defense * 1.2 + ability.speed + ability.skill + ability.strength + ability.spirit) / 6
+            }
+        ];
+        return position;
     },
     birthday: function () {
         return '19' + String(this.score(80, 99)) + '/' + String(Math.ceil(Math.random() * 12)) + '/' + String(Math.ceil(Math.random() * 28));
@@ -33,16 +116,7 @@ export function loadPlayers() {
                 height: fake.score(160, 200),
                 club: clubs[Math.ceil(Math.random() * 15)].code,
                 uniformNo: fake.score(1, 22),
-                position: [
-                    {
-                        name: fake.position(),
-                        score: fake.score(50, 100)
-                    },
-                    {
-                        name: fake.position(),
-                        score: fake.score(50, 100)
-                    }
-                ],
+                position: fake.position(),
                 star: fake.score(1, 3),
                 level: 1,
                 type: playerTypes[Math.ceil(Math.random() * 2)].code,
